dc7177e67aa5793d278238563dd097dc52eea2cd
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-04-30 14:07:36 -0400

    python

---
+++ variables.py
@@ -0,0 +1,35 @@
+#!usr/bin/python
+
+#Single Line Comments : #,''," ",''' ''',&""" """
+#Multiline Comments   : ''' ''',&""" """
+
+'''
+Name of the program is Variables
+Author  : Chaithanya
+Platform : windows
+'''
+
+'Rule of creating variables'
+"""
+1.A-Z
+2.a-z
+3.A-Za-z
+4._Special Character
+"""
+
+#Note : A variable can not start with integer value(0-9)
+
+'Creating Variables in python'
+
+FIRSTNAME = 'Chaitanya'
+middlename= "praveen"
+LastName  = 'Thumma'
+_Software = 'Python'
+_005      = 3.6
+
+'Accessing variables in python'
+print(FIRSTNAME)
+print(middlename)
+print(LastName )
+print(_Software )
+print(_005 )
