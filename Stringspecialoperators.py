03bc75ea68e31fd742cfa4ab07ca15291408136e
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-04-30 17:10:47 -0400

    python

---
+++ Stringspecialoperators.py
@@ -0,0 +1,17 @@
+str1 = "Hello world"
+str2 = 'This is an example string'
+
+print(str1,len(str1))
+print(str1[1])
+
+print(str1[10])
+print(str1[-2])
+print(str1[-11])
+
+print(str1[0:6])
+
+print(str1,type(str1),len(str1),id(str1))
+
+print('Range slice & left to Right Indexing', str1[0])
+
+print('Range slice & Right to left Indexing', str1[-1])
